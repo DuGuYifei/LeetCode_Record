0c4db8aa6e53af7afc1af80b62684f8b06838efb
DuGuYifei <liuyifei97@163.com>
2022-08-04 00:20:43 +0200

    2022-8-4

--- 899.有序队列.cpp
+++ 899.有序队列.cpp
@@ -5,11 +5,24 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    string orderlyQueue(string s, int k) {
-
+    string orderlyQueue(string s, int k)
+    {
+        if (k != 1)
+        {
+            sort(s.begin(), s.end());
+            return s;
+        }
+        string ans = s;
+        int n = s.size();
+        for (int i = 0; i < n; i++)
+        {
+            s = s.substr(1, n - 1) + s[0];
+            ans = min(s, ans);
+        }
+        return ans;
     }
 };
 // @lc code=end
-
