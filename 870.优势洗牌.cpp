c8ca1e0ba997c7a2874a265effe08503c07a38ec
DuGuYifei <liuyifei97@163.com>
2022-10-07 23:51:49 +0200

    2022-10-7

---
+++ 870.优势洗牌.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=870 lang=cpp
+ *
+ * [870] 优势洗牌
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> advantageCount(vector<int>& nums1, vector<int>& nums2) {
+
+    }
+};
+// @lc code=end
+
